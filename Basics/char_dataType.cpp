e6f2cabbc3bf8cb6d36d2e73e7d33158009f899c
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-04-04 09:50:34 +0530

    intro_topics

--- Basics/char_dataType.cpp
+++ Basics/char_dataType.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 using namespace std;
 int main() {
+    // char ch;
+    // cin>>ch;
+    // cout<<ch;
+    // char d = 'a';
+    // cout<<d<<"\n";
     char c;
     cin>>c;
-    cout<<c;
-    char d = 'a';
-    cout<<d;
+    cout<<(int)c<<"\n";
+    int x;
+    cin>>x;
+    char ch = (char)x;
+    cout<<ch;
 }
---
+++ Basics/hello_world.cpp
@@ -0,0 +1,14 @@
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     cout<<"Hello World!";
+// }
+
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"Hello Alok"<<"\n"<<"Hello Advik";
+    cout<<'Hello Alok'; // this line will not work because of single inverted commas
+    cout<<"Hello Alok";
+}
+
